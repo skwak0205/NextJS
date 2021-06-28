513df5299b59c4f5125424614a70e5ca6bc77ba8
Soojung Kwak <gsj2464@gmail.com>
2021-06-29 08:50:35 +0900

    detail page 추가

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MettupDetails() {
+  return (
+    <MeetupDetail
+      image='https://images.unsplash.com/photo-1546874177-9e664107314e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VvdWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+      title='First Meetup'
+      address='Seoul, South Korea'
+      description='This is a first meetup'
+    />
+  );
+}
+
+export default MettupDetails;
