d4ba83b18bf460a0419095671b2a8e1a8e983fa9
Soojung Kwak <gsj2464@gmail.com>
2021-06-29 10:11:45 +0900

    meetups 불러오기

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,6 @@
+import { MongoClient } from 'mongodb';
+
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A first Meetup',
-    image:
-      'https://images.unsplash.com/photo-1546874177-9e664107314e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VvdWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
-    address: 'Seoul, South Korea',
-    description: 'This is a first meetup!',
-  },
-
-  {
-    id: 'm2',
-    title: 'A second Meetup',
-    image:
-      'https://images.unsplash.com/photo-1546874177-9e664107314e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VvdWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
-    address: 'Seoul, South Korea',
-    description: 'This is a second meetup!',
-  },
-];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -26,11 +8,23 @@ function HomePage(props) {
 
 // pre-rendering 중에 실행됨
 export async function getStaticProps() {
-  // fetch data from an API
+  const client = await MongoClient.connect('');
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
 
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 10,
   };
