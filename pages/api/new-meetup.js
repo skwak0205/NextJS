c291800dbc82b7aac65194fadc10707fb2cbdbdf
Soojung Kwak <gsj2464@gmail.com>
2021-06-29 09:55:14 +0900

    mongodb 연결

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,22 @@
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect('');
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne({ data });
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
